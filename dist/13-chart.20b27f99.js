1801885ac255150ab02ce6ca01df850c1a02e51b
Kate S <ksliney@gmail.com>
2018-11-13 05:40:22 -0500

    bars

--- dist/13-chart.20b27f99.js
+++ dist/13-chart.20b27f99.js
@@ -112,23 +112,23 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   });
   var margin = {
     top: 30,
-    left: 30,
+    left: 50,
     right: 30,
     bottom: 30
   };
-  var height = 700 - margin.top - margin.bottom;
-  var width = 600 - margin.left - margin.right;
+  var height = 600 - margin.top - margin.bottom;
+  var width = 700 - margin.left - margin.right;
   var svg = d3.select("#chart13").append('svg').attr('height', height + margin.top + margin.bottom).attr('width', width + margin.left + margin.right).append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-  var widthScale = d3.scaleLinear().domain([0, 10]).range([0, 500]);
+  var widthScale = d3.scaleLinear().domain([0, 10]).range([0, width]);
   var colorScale = d3.scaleOrdinal().range(["blue", "red", "green"]);
 
   function ready(datapoints) {
     console.log("Data is", datapoints);
-    var xPositionScale = d3.scaleLinear().domain([0, 15]).range([0, 550]);
+    var xPositionScale = d3.scaleLinear().domain([0, 15]).range([0, width]);
     var names = datapoints.map(function (d) {
       return d.name;
     });
-    var yScale = d3.scaleBand().domain(names).range([600, 0]);
+    var yScale = d3.scaleBand().domain(["Stevie", "Nicholas", "Bubbletree", "Particle", "Jumpup", "Parlay", "Hio"]).range([height, 0]);
     svg.selectAll("rect").data(datapoints).enter().append("rect").attr("width", function (d) {
       return widthScale(d.hamburgers);
     }).attr("height", 50).attr("fill", function (d) {
@@ -136,10 +136,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
     }).attr("y", function (d, i) {
       return yScale(d.name);
     }).attr("opacity", 0.5);
+    /* Set up axes */
+
     var xAxis = d3.axisBottom(xPositionScale).ticks(10);
-    svg.append("g").attr("class", "axis x-axis").attr("transform", "translate(0," + 600 + ")").call(xAxis).lower();
-    svg.append("text").attr("x", 200).attr("y", 650).text("hamburgers consumed");
-    var yAxis = d3.axisLeft(names).ticks(7);
+    svg.append('g').attr('class', 'axis x-axis').attr('transform', 'translate(0,' + height + ')').call(xAxis);
+    var yAxis = d3.axisLeft(yScale).ticks(7);
+    svg.append('g').attr('class', 'axis y-axis').call(yAxis);
     svg.append("g").attr("class", "axis y-axis").call(yAxis);
   }
 })();
